c1eee3c19286b06c8b075e55f4a149aaa3147b31
Georgi Terziev <g.d.terziev@gmail.com>
2016-03-03 16:45:12 +0100

    Add gaussianDer function

---
+++ Assignment 3/gaussianDer.m
@@ -0,0 +1,5 @@
+function Gd = gaussianDer(sigma_i,sigma_d,length)
+    x = linspace(-(length-1)/2,(length-1)/2,length);
+    G = fspecial('gaussian',[1 length],sigma_i)
+    Gd = - x .* G ./ (sigma_d*sigma_d);
+end
