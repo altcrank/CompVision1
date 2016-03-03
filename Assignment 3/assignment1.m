8ae9a8157b692b3326f3bc3a76ef0d6264de5a34
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2016-03-03 21:03:03 +0100

    assignment1.m
    .

--- Assignment 3/assignment1.m
+++ Assignment 3/assignment1.m
@@ -1,39 +1,47 @@
 function assignment1
     
-    img = im2double(imread('person_toy/00000001.jpg'));
+    colorImg = im2double(imread('person_toy/00000001.jpg'));
+    img = colorImg;
     if (length(img(1,1,:)) == 3)
         img = rgb2gray(img);
     end
     
     figure('units','normalized','outerposition',[0 0 1 1]);
     
-    [~,rows,cols] = harris(img, 0.0000005, 15, 2);
+    %[~,rows,cols] = harris(img, 1*10^(-7), 15, 2);
+    %[~,rows,cols] = harris(img, 1*10^(-6), 15, 1);
+    
+    
+    [~,rows,cols] = harris(img, 1*10^(-6), 15, 2);
     subplot(2,2,1);
-    imshow(img);
+    imshow(colorImg);
     hold on;
-    plot(cols, rows, 'g*');
+    plot(cols, rows, 'b*');
     hold off;
     
-    [~,rows,cols] = harris(img, 0.0000005, 15, 2);
+    [~,rows,cols] = harris(img, 1*10^(-7), 15, );
     subplot(2,2,2);
-    imshow(img);
+    imshow(colorImg);
     hold on;
-    plot(cols, rows, 'g*');
+    plot(cols, rows, 'b*');
     hold off;
     
-    [~,rows,cols] = harris(img, 0.0000005, 15, 2);
+    [~,rows,cols] = harris(img, 1*10^(-6), 15, 2);
     subplot(2,2,3);
-    imshow(img);
+    imshow(colorImg);
     hold on;
-    plot(cols, rows, 'g*');
+    plot(cols, rows, 'b*');
     hold off;
     
-    [~,rows,cols] = harris(img, 0.0000005, 15, 2);
+    [~,rows,cols] = harris(img, 1*10^(-6), 15, 1);
     subplot(2,2,4);
-    imshow(img);
+    imshow(colorImg);
     hold on;
-    plot(cols, rows, 'g*');
+    plot(cols, rows, 'b*');
     hold off;
+    
+    disp('Press any key to continue with the other image');
+    %pause;
     
 %harris('pingpong/0000.jpeg', 0.0000005, 10, 2);
     
