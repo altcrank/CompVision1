9d13b06627f5644b14cebb87b4e39df850dcfd2a
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2016-03-06 22:38:54 +0100

    assign1

--- Assignment 3/assignment1.m
+++ Assignment 3/assignment1.m
@@ -1,50 +1,32 @@
 function assignment1
-    
+    figure('units','normalized','outerposition',[0 0 1 1]);
+
     colorImg = im2double(imread('person_toy/00000001.jpg'));
     img = colorImg;
     if (length(img(1,1,:)) == 3)
         img = rgb2gray(img);
     end
     
-    figure('units','normalized','outerposition',[0 0 1 1]);
-    
-    %[~,rows,cols] = harris(img, 1*10^(-7), 15, 2);
-    %[~,rows,cols] = harris(img, 1*10^(-6), 15, 1);
-    
-    
-    [~,rows,cols] = harris(img, 1*10^(-6), 15, 2);
-    subplot(2,2,1);
+    [~,rows,cols] = harris(img, 6*10^(-6), 15, 1.5);
+    subplot(1,2,1);
     imshow(colorImg);
+    title('sigma = 1.5, threshold = 6*10^{(-6)}, window = 15 x 15');
     hold on;
     plot(cols, rows, 'b*');
     hold off;
+  
+    colorImg = im2double(imread('pingpong/0000.jpeg'));
+    img = colorImg;
+    if (length(img(1,1,:)) == 3)
+        img = rgb2gray(img);
+    end
     
-    [~,rows,cols] = harris(img, 1*10^(-7), 15, 1);
+    [~,rows,cols] = harris(img, 7*10^(-6), 15, 1.5);
+    subplot(1,2,2);
+    imshow(colorImg);
+    title('sigma = 1.5, threshold = 7*10^{(-6)}, window = 15 x 15');
+    hold on;
+    plot(cols, rows, 'b*');
+    hold off;
 
-    subplot(2,2,2);
-    imshow(colorImg);
-    hold on;
-    plot(cols, rows, 'b*');
-    hold off;
-    
-    [~,rows,cols] = harris(img, 1*10^(-6), 15, 2);
-    subplot(2,2,3);
-    imshow(colorImg);
-    hold on;
-    plot(cols, rows, 'b*');
-    hold off;
-    
-    [~,rows,cols] = harris(img, 1*10^(-6), 15, 1);
-    subplot(2,2,4);
-    imshow(colorImg);
-    hold on;
-    plot(cols, rows, 'b*');
-    hold off;
-    
-    disp('Press any key to continue with the other image');
-    %pause;
-    
-%harris('pingpong/0000.jpeg', 0.0000005, 10, 2);
-    
-    
 end
