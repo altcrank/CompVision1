68b2b43d5118f72cdf77d051092358c3f5249a82
Georgi Terziev <g.d.terziev@gmail.com>
2016-03-18 23:38:51 +0100

    Final Project Add a function that quantizes an image

--- Final Project/quantize_feature.m
+++ Final Project/quantize_feature.m
@@ -1,11 +1,17 @@
-function word = quantize_feature(vocab,feature)
+function [word,index] = quantize_feature(vocab,feature)
+    % Because vocabulary was used with DOUBLE data
+    feat = double(feature);
+    % Set first to be closest
     word = vocab(:,1);
-    smallest_dist = norm(vocab(:,1) - feature);
+    index = 1;
+    smallest_dist = norm(vocab(:,1) - feat);
+    % Find actual closest
     for w = 2:size(vocab,2)
-        dist = norm(vocab(:,w) - feature);
+        dist = norm(vocab(:,w) - feat);
         if dist < smallest_dist
             smallest_dist = dist;
             word = vocab(:,w);
+            index = w;
         end
     end
 end
