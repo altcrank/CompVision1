1ee7d89d3411a747b4b5ea6b6ee7692db3bba4ca
Georgi Terziev <g.d.terziev@gmail.com>
2016-03-28 15:51:30 +0200

    Final Project. Revert changes, so that, training on subsampled set is possible

--- Final Project/quantize_image.m
+++ Final Project/quantize_image.m
@@ -1,7 +1,8 @@
-function image_vector = quantize_image(vocab,image_descr)
+function image_vector = quantize_image(vocab,image,type,step)
+    [~,descr] = sift(image,type,step);
     image_vector = zeros([size(vocab,2) 1]);
-    for d = 1:size(image_descr,2)
-        [~,index] = quantize_feature(vocab,image_descr(:,d));
+    for d = 1:size(descr,2)
+        [~,index] = quantize_feature(vocab,descr(:,d));
         image_vector(index) = image_vector(index) + 1;
     end
     image_vector = double(image_vector);
