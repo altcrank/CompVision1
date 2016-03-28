f2b97baa08afbcce3e806fa522e30ccd8aacb163
Georgi Terziev <g.d.terziev@gmail.com>
2016-03-28 15:53:52 +0200

    Final Project Fix syntax error from last commit

--- Final Project/quantize_images.m
+++ Final Project/quantize_images.m
@@ -1,7 +1,7 @@
 function quantized = quantize_images(vocab,images,type,step)
     quantized = {};
     for i = 1:size(images,2)
--        class = images{i};
+        class = images{i};
         quantized_class = {};
         for j = 1:size(class,2)
             quantized_class = [quantized_class,quantize_image(vocab,class{j},type,step)];
